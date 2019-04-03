3af6b33d8af0f2431b3be07b81c242646a7b3044
ErickGiffoni <giffoni.erick@gmail.com>
2019-04-03 10:30:04 -0300

    #54 - Beggin Telegram.py

---
+++ classes/Telegram.py
@@ -0,0 +1,19 @@
+import mongoengine
+import User
+
+class Telegram(mongoengine.Document):
+    username = mongoengine.StringField()
+    token = mongoengine.StringField()
+
+    users = list() #lista de usuarios ?
+
+    def getNotifications():
+        pass
+
+    def sendNotifications():
+        pass
+
+#testing
+chat1 = Telegram()
+chat1.username = "chatopsMDS"
+chat1.save() #salva no banco de dados
