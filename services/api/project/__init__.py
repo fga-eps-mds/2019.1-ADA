48248079269fb435eeb0979feeb7bb3ef4b14c76
João Vitor <joaovytor0@gmail.com>
2019-04-02 23:45:44 -0300

    #52 Add pymongo dependencies and development config

--- services/api/project/__init__.py
+++ services/api/project/__init__.py
@@ -1,15 +1,28 @@
-from flask import Flask, jsonify
+# services/users/project/__init__.py
 
-# instatiates flask app
 
+import os
+from flask import Flask, jsonify, request
+# from flask_mongoengine import MongoEngine
+from flask_pymongo import PyMongo
+import sys
+
+
+
+# instantiate the app
 app = Flask(__name__)
 
-# sets as development config
-app.config.from_object('project.config.DevelopmentConfig')
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+mongo = PyMongo(app)  # new
 
 
-@app.route('/ada/ping', methods=['GET'])
+@app.route('/api/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
-        'status': 'ok',
+        'status': 'success',
+        'message': 'pong!'
     })
--- services/api/project/config.py
+++ services/api/project/config.py
@@ -1,18 +1,26 @@
+# services/users/project/config.py
+
+
+import os  # new
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    MONGO_TRACK_MODIFICATIONS = False  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    MONGO_URI = 'mongodb://mongo:27017/api'
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    MONGO_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    MONGO_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
