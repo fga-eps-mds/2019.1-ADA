4f79e77b3e3b78550231a3e3a576fbea890ac1fb
ErickGiffoni <giffoni.erick@gmail.com>
2019-04-05 11:37:02 -0300

    #54 - Update __init__.py - get method for User
    Co-authored-by: Joao Vitor <joaovytor0@gmail.com>
    Co-authored-by: Caio Fernandes <caiovf13@gmail.com>

--- services/api/project/__init__.py
+++ services/api/project/__init__.py
@@ -22,6 +22,8 @@ db = MongoEngine()  # new
 db.init_app(app)
 
 class User(db.Document):
+    meta = {'collection': 'users'}
+    
     name = db.StringField()
     username = db.StringField()
     telegramID = db.StringField()
@@ -31,10 +33,10 @@ class User(db.Document):
     # def __init__(self):
     #    """Construtor User"""
     # nao e necessaerio ter metodo __init__(https://stackoverflow.com/questions/38363726/mongoengine-attributeerror)
-    def getUser(self):
+    def get_user(self):
         """ Retorna o trio nome, user e telegramID"""
         return  self.name, self.last_name, self.username, self.telegramID, self.is_bot
-    def newUser(self, name, last_name, username, telegramID, is_bot):
+    def new_user(self, name, last_name, username, telegramID, is_bot):
         """Cria um novo usuario e o salva no banco de dados Telegram_User_DataBase"""
         self.name = name
         self.last_name = last_name
@@ -43,6 +45,10 @@ class User(db.Document):
         self.is_bot = is_bot
         self.save() # acho que isso aqui que realmente salva o usuario no database
 
+    def find_user_by_username(self, username):
+        user = self
+        user = self.objects(username=username)
+        return user
 
 @app.route('/api/ping', methods=['GET'])
 def ping_pong():
@@ -63,3 +69,35 @@ def add_user():
         'status': 'success',
         'message': 'user added!'
     })
+
+@app.route("/api/find/user/<username>", methods=["GET"])
+def find_user_by_username(username):
+    User.objects.get_or_404(username=username)
+    return jsonify({
+        "status": "success",
+        "message": "user finded"
+    })
+    #search = User()
+    #search = search.find_user_by_username(username)
+    #if user:
+    #    return jsonify({
+    #    "status": "success",
+    #    "message": "user finded"
+    #})
+    #return jsonify({
+    #    "status": "failed",
+    #    "message": "user not found"
+    #})
+
+#@app.route('/api/getuser', methods=['GET'])
+#def get_user():
+#    user = User()
+#    user = user.find_user_by_username("erickdev")
+#    first_name, last_name, username, telegramID, is_bot = user.getUser()
+#    return jsonify({
+#        'name': first_name,
+#        'last_name': last_name,
+#        'username': username,
+#        'telegramID': telegramID,
+#        'is_bot': is_bot
+#    })
