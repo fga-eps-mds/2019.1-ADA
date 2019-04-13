9b72682805ce60d3ae4dde1dae3d022fba38e23a
guilherme-mendes <guimendesp12@gmail.com>
2019-04-13 00:34:08 -0300

    #89 Update custom actions with gitlab process build status
    Co-authored-by: Caio Fernandes <caiovf13@gmail.com>

--- ada/actions/actions.py
+++ ada/actions/actions.py
@@ -3,12 +3,21 @@ from rasa_core_sdk.events import SlotSet
 import requests
 import random
 
-class ActionTest(Action):
+class ActionGetBuild(Action):
    def name(self):
-      return "action_test"
+      return "get_build"
 
    def run(self, dispatcher, tracker, domain):
         try:
-          dispatcher.utter_message("Mensagem enviada por uma custom action.")
+          dispatcher.utter_message("Status das últimas builds do processo:")
+          headers = {
+              'Content-Type': 'application/json',
+              'PRIVATE-TOKEN': '[GITLAB_TOKEN]',
+          }
+          response = requests.get('https://gitlab.com/api/v4/projects/adabot%2Fada-gitlab/jobs', headers=headers)
+          requests_build = response.json()
+          for i in [0,1,2]:
+            dispatcher.utter_message(requests_build[i]['web_url'])
+            dispatcher.utter_message(requests_build[i]['status'])
         except ValueError:
           dispatcher.utter_message(ValueError)
