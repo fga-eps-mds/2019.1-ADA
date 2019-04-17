a512dfe692361335da50c8c71e8bf554f6f4b61c
guilherme-mendes <guimendesp12@gmail.com>
2019-04-17 14:45:09 -0300

    Update the response flow of bot requests

--- ada/actions/action_build_stage.py
+++ ada/actions/action_build_stage.py
@@ -1,8 +1,6 @@
 from rasa_core_sdk import Action
 import requests
 import os
-import random
-from rasa_core_sdk.events import SlotSet
 
 GITLAB_SERVICE_URL = os.getenv("GITLAB_SERVICE_URL", "")
 
@@ -14,33 +12,29 @@ class BuildStage(Action):
     def run(self, dispatcher, tracker, domain):
         is_there_any_build = False
         try:
-          dispatcher.utter_message("Certo! Encontrei a seguinte build no seu \
-                                    repositório\n\n:")
-          headers = {
-              'Content-Type': 'application/json',
-          }
-          project_owner = "adabot"
-          project_name = "ada-gitlab"
-          response = requests.get(GITLAB_SERVICE_URL + "jobs/{project_owner}/{project_name}".format(
-              project_owner=project_owner, project_name=project_name), headers=headers)
-          requests_build = response.json()
-        #   for i in range(3):
-          dispatcher.utter_message(f"A build {requests_build[0]['name']} da \
-                                     pipeline {requests_build[0]['pipeline_ref']} \
-                                     está no estágio {requests_build[0]['stage']}.\n\n\
-                                     O status atual dela é {requests_build[0]['status']}.")
-          dispatcher.utter_message(f"Você pode encontrar mais informações sobre essa \
-                                    build no seguinte endereço:\n\
-                                    {requests_build[i]['web_url']}")
-          #dispatcher.utter_message(requests_build[0]['name'])
-          #dispatcher.utter_message(requests_build[0]['stage'])
-          #dispatcher.utter_message(requests_build[0]['status'])
-          #dispatcher.utter_message(requests_build[i]['pipeline']["ref"])
-          # dispatcher.utter_message(requests_build[i]['web_url'])
-          is_there_any_build = True
+            dispatcher.utter_message("Certo! Encontrei a seguinte build no seu "
+                                     "repositório.")
+            headers = {'Content-Type': 'application/json'}
+            project_owner = "adabot"
+            project_name = "ada-gitlab"
+            response = requests.get(GITLAB_SERVICE_URL +
+                                    "build/{project_owner}/{project_name}"
+                                    .format(project_owner=project_owner, 
+                                    project_name=project_name), headers=headers)
+            requests_build = response.json()
+            dispatcher.utter_message("A build {name} da "
+                                    "pipeline {pipeline_ref} "
+                                    "está no estágio {stage}."
+                                    .format(name=requests_build['name'].capitalize(),
+                                            pipeline_ref=requests_build['pipeline']['ref'].capitalize(),
+                                            stage=requests_build['stage'].capitalize()))
+            dispatcher.utter_message("O status atual dela é {status}."
+                                    .format(status=requests_build['status'].capitalize()))
+            dispatcher.utter_message("Para visualizar essa build no GitLab acesse o link {web_url}"
+                                    .format(web_url=requests_build['web_url']))
+            is_there_any_build = True
         except ValueError:
             dispatcher.utter_message(ValueError)
             if(not is_there_any_build):
-                default = "Não há build's em andamento, \
-                mas continuo te informando.\n"
+                default = "Não há build's em andamento, mas continuo te informando."
                 dispatcher.utter_message(default)
