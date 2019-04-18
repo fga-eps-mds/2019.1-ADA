abbc355f88280efc9163b62337fefc473df1d647
guilherme-mendes <guimendesp12@gmail.com>
2019-04-18 13:31:48 -0300

    #109 Update build response

--- ada/actions/action_build_stage.py
+++ ada/actions/action_build_stage.py
@@ -15,39 +15,50 @@ class BuildStage(Action):
             dispatcher.utter_message("Certo! Encontrei a build mais "
                                      "recente do seu repositório.")
             headers = {'Content-Type': 'application/json'}
-            project_owner = "adabot"
-            project_name = "ada-gitlab"
+            project_owner = "gitlab-org"
+            project_name = "gitlab-ee"
             response = requests.get(GITLAB_SERVICE_URL +
                                     "build/{project_owner}/{project_name}/jobs"
-                                    .format(project_owner=project_owner, 
-                                    project_name=project_name), headers=headers)
+                                    .format(project_owner=project_owner,
+                                            project_name=project_name),
+                                    headers=headers)
             requests_build = response.json()
 
             if requests_build['status'] == "success":
-                status = "E está passando nos critérios de aceitação adotadas pela Build."
+                status = "✅"
+            elif requests_build['status'] == "failed":
+                status = "❌"
             else:
-                status = "E não está passando nos critérios de aceitação adotadas pela Build."
-            
-            if requests_build['stage'] == "test":
-                stage = "de teste"
-            elif requests_build['stage'] == "build":
-                stage = "de construção"
-            elif requests_build['stage'] == "deploy":
-                stage = "de implementação"
-            
+                status = "🔄"
 
-            dispatcher.utter_message("A build {name} do "
-                                    "serviço {ref} "
-                                    "está no estágio {stage}."
-                                    .format(name=requests_build['name'].capitalize(),
-                                            ref=requests_build['ref'],
-                                            stage=stage))
-            dispatcher.utter_message("{status}".format(status=status))
-            dispatcher.utter_message("Para visualizar essa build no GitLab acesse o link {web_url}"
-                                    .format(web_url=requests_build['web_url']))
+            if requests_build['status1'] == "success":
+                status1 = "✅"
+            elif requests_build['status1'] == "failed":
+                status1 = "❌"
+            else:
+                status1 = "🔄"
+
+            dispatcher.utter_message('A build #{id} da '
+                                     'branch {branch}, '
+                                     'commit "{commit}", '
+                                     'está no estágio de "{stage}".'
+                                     .format(id=requests_build['id'],
+                                             commit=requests_build['commit'],
+                                             branch=requests_build['branch'],
+                                             stage=requests_build['stage']))
+            dispatcher.utter_message("Os passos da build são:\n"
+                                     "{status} {name}\n{status1} {name1}"
+                                     .format(status=status,
+                                             name=requests_build['name'],
+                                             status1=status1,
+                                             name1=requests_build['name1']))
+            dispatcher.utter_message("Para visualizar sua build "
+                                     "no GitLab acesse o link {web_url}"
+                                     .format(web_url=requests_build['web_url']))
             is_there_any_build = True
         except ValueError:
             dispatcher.utter_message(ValueError)
             if(not is_there_any_build):
-                default = "Não há build's em andamento, mas continuo te informando."
+                default = "Não há build's em andamento, "
+                "mas continuo te informando."
                 dispatcher.utter_message(default)
