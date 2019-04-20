24222e30be8ac67938f5594227c0a7ecfd25bed1
guilherme-mendes <guimendesp12@gmail.com>
2019-04-20 00:02:33 -0300

    #109 Update conversation flow with n build steps

--- ada/actions/action_build_stage.py
+++ ada/actions/action_build_stage.py
@@ -2,6 +2,7 @@ from rasa_core_sdk import Action
 import requests
 import os
 
+
 GITLAB_SERVICE_URL = os.getenv("GITLAB_SERVICE_URL", "")
 
 
@@ -16,46 +17,45 @@ class BuildStage(Action):
                                      "recente do seu repositório.")
             headers = {'Content-Type': 'application/json'}
             project_owner = "gitlab-org"
-            project_name = "gitlab-ee"
+            project_name = "gitaly"
             response = requests.get(GITLAB_SERVICE_URL +
-                                    "build/{project_owner}/{project_name}/jobs"
+                                    "build/{project_owner}/{project_name}"
                                     .format(project_owner=project_owner,
                                             project_name=project_name),
                                     headers=headers)
-            requests_build = response.json()
+            received_build = response.json()
 
-            if requests_build['status'] == "success":
-                status = "✅"
-            elif requests_build['status'] == "failed":
-                status = "❌"
-            else:
-                status = "🔄"
+            text_message = "Os passos da build são:\n"
 
-            if requests_build['status1'] == "success":
-                status1 = "✅"
-            elif requests_build['status1'] == "failed":
-                status1 = "❌"
-            else:
-                status1 = "🔄"
+            for i, item in enumerate(received_build):
 
-            dispatcher.utter_message('A build #{id} da '
-                                     'branch {branch}, '
+                if received_build[i]['status'] == "success":
+                    status = "✅"
+                elif received_build[i]['status'] == "failed":
+                    status = "❌"
+                else:
+                    status = "🔄"
+
+                text_message += "{status} {job_name}\n" \
+                                .format(status=status,
+                                        job_name=received_build[i]['job_name'])
+
+            dispatcher.utter_message('A build #{job_id} '
+                                     'da branch {branch}, '
                                      'commit "{commit}", '
                                      'está no estágio de "{stage}".'
-                                     .format(id=requests_build['id'],
-                                             commit=requests_build['commit'],
-                                             branch=requests_build['branch'],
-                                             stage=requests_build['stage']))
-            dispatcher.utter_message("Os passos da build são:\n"
-                                     "{status} {name}\n{status1} {name1}"
-                                     .format(status=status,
-                                             name=requests_build['name'],
-                                             status1=status1,
-                                             name1=requests_build['name1']))
+                                     .format(job_id=received_build[0]['job_id'],
+                                             branch=received_build[0]['branch'],
+                                             commit=received_build[0]['commit'],
+                                             stage=received_build[0]['stage']))
+
+            dispatcher.utter_message(text_message)
+
             dispatcher.utter_message("Para visualizar sua build "
-                                     "no GitLab acesse o link {web_url}"
-                                     .format(web_url=requests_build
-                                             ['web_url']))
+                                     "no GitLab acesse o link {pipeline_url}"
+                                     .format(pipeline_url=received_build \
+                                     [0]['web_url']))
+
             is_there_any_build = True
         except ValueError:
             dispatcher.utter_message(ValueError)
