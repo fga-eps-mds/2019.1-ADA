0d8da16d5d8015d6320734fd5d9fdb25453717fa
João Pedro <isudjoao@gmail.com>
2019-04-26 18:43:06 -0300

    #132 Fix bug with user
    Co-authored-By: Guilherme Mendes <guimendesp12@gmail.com>

--- ada/actions/set_user.py
+++ ada/actions/set_user.py
@@ -18,49 +18,42 @@ class ActionSetUser(Action):
 
             message = tracker.latest_message.get('text')
             message = message.split()
-            username = message[len(message)-1]
+            project_owner = message[len(message)-1]
 
             headers = {"Content-Type": "application/json"}
-            project_owner = username
             get_repository = GITLAB_SERVICE_URL + \
                 "user/{project_owner}".format(
                     project_owner=project_owner)
-            print(get_repository, file=sys.stderr)
             response = requests.get(
                 get_repository, headers=headers)
             received_repositories = response.json()
-
             options = []
             for i, item in enumerate(received_repositories['repositories']):
                 options.append((item, item))
 
             repositories_buttons = self.build_buttons(options)
-            repositories_buttons.sort()
             lista = [repositories_buttons[x:x+2]
                      for x in range(0, len(repositories_buttons), 2)]
-            # dispatcher.utter_message(
-            #     "Escolha o repositório que deseja gerenciar:")
 
             dispatcher.utter_message(
-                "Olá {user}, obrigado por confiar em mim para te ajudar nos seus trabalhos.".format(user=username))
+                "Olá {user}, obrigado por confiar em mim para te ajudar nos seus trabalhos.".format(user=project_owner))
             for item in lista:
                 dispatcher.utter_button_message('Qual repositório você quer que eu fique responsavél?',
                                                 item,
                                                 button_type="custom")
 
-            return [SlotSet('usuario', username)]
+            return [SlotSet('usuario', project_owner)]
 
         except KeyError:
             dispatcher.utter_message(
-                "Não consegui encontrar o nome de usuário {user} no banco de dados do gitlab.".format(user=username))
+                "Não consegui encontrar o nome de usuário {user} no banco de dados do gitlab.".format(user=project_owner))
         except IndexError:
             dispatcher.utter_message(
-                "Não consegui encontrar o nome de usuário {user} no banco de dados do gitlab.".format(user=username))
+                "Não consegui encontrar o nome de usuário {user} no banco de dados do gitlab.".format(user=project_owner))
 
     def build_buttons(self, button_values):
         buttons = []
         button = {}
-        button_values.sort()
         for item in button_values:
             button['title'] = item[0]
             button['payload'] = "meu repositório é " + item[1]
