97686f622a17a16412b20ecafd4fc989f95cd498
ErickGiffoni <giffoni.erick@gmail.com>
2019-05-02 15:42:25 -0300

    #135 update set_user.py
    Co-authored-by: Joao Pedro <isudjoao@gmail.com>

--- ada/actions/set_user.py
+++ ada/actions/set_user.py
@@ -1,5 +1,14 @@
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
+import os
+import requests
+import telegram
+import json
+from urllib3.exceptions import NewConnectionError
+from requests.exceptions import HTTPError
+
+GITLAB_SERVICE_URL = os.environ.get("GITLAB_SERVICE_URL", "")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "")
 
 
 class ActionSetUser(Action):
@@ -8,17 +17,81 @@ class ActionSetUser(Action):
 
     def run(self, dispatcher, tracker, domain):
         try:
-            # tracker_state = tracker.current_state()
-            # sender_id = tracker_state['sender_id']
+            tracker_state = tracker.current_state()
+            sender_id = tracker_state['sender_id']
+
             message = tracker.latest_message.get('text')
             message = message.split()
-            username = message[len(message)-1]
+            project_owner = message[len(message)-1]
+
+            bot = telegram.Bot(token=ACCESS_TOKEN)
+            bot.send_message(chat_id=sender_id,
+                             text="Ok, {user}, anotei aqui, "
+                                  "vou dar uma conferida "
+                                  "pra ver se está tudo certo."
+                                  .format(user=project_owner))
+            headers = {"Content-Type": "application/json"}
+            repo_names = self.build_buttons(project_owner, headers)
+            reply_markup = telegram.InlineKeyboardMarkup(repo_names)
+            bot.send_message(chat_id=sender_id,
+                             text="Encontrei esses repositórios na sua "
+                                  "conta. Qual você quer que eu "
+                                  "monitore? Clica nele!",
+                             reply_markup=reply_markup)
+            self.save_user_to_db(headers, project_owner, sender_id)
+            return [SlotSet('usuario', project_owner)]
+        except KeyError:
             dispatcher.utter_message(
-                "Seu nome de usuário é: {user}.".format(user=username))
-            dispatcher.utter_message("Agora me fala"
-                                     + " o nome do seu repositório, "
-                                     + " mas não se esqueça que de escrever"
-                                     + "repositório: na frente")
-            return [SlotSet('usuario', username)]
+                "Não consegui encontrar o seu usuário no GitLab, "
+                "por favor verifique ele e me manda novamente.")
+        except IndexError:
+            dispatcher.utter_message(
+                "Não consegui encontrar o seu usuário no GitLab, "
+                "por favor verifique ele e me manda novamente.")
+        except NewConnectionError:
+            dispatcher.utter_message(
+                "Estou com problemas para me conectar, me manda "
+                "mais uma mensagem pra ver se dessa vez dá certo.")
         except ValueError:
-            dispatcher.utter_message(ValueError)
+            dispatcher.utter_message(
+                "Estou com problemas para me conectar, me manda "
+                "mais uma mensagem pra ver se dessa vez dá certo.")
+        except HTTPError:
+            existent_user = "Eu vi aqui que você já cadastrou o usuário "\
+                            "do GitLab. Sinto muito, mas no momento "\
+                            "não é possível cadastrar um novo usuário"\
+                            " do GitLab ou alterá-lo."
+            dispatcher.utter_message(existent_user)
+
+    def build_buttons(self, project_owner, headers):
+        get_repository = GITLAB_SERVICE_URL + \
+            "user/{project_owner}".format(
+                project_owner=project_owner)
+        response = requests.get(
+            url=get_repository, headers=headers)
+
+        received_repositories = response.json()
+        buttons = []
+        for repositorio in received_repositories["repositories"]:
+            project_name = repositorio.split('/')
+            project_name = project_name[-1]
+            buttons.append(telegram.InlineKeyboardButton(
+                text=project_name,
+                callback_data="meu repositorio é " + repositorio))
+        repo_names = [buttons[i:i+2] for i in range(0, len(buttons), 2)]
+        return repo_names
+
+    def save_user_to_db(self, headers, project_owner, sender_id):
+        get_user_id = GITLAB_SERVICE_URL + \
+            "user/id/{project_owner}".format(
+                project_owner=project_owner)
+        response = requests.get(
+            url=get_user_id, headers=headers)
+        user_id = response.json()
+        db_json = {"gitlab_user": project_owner, "chat_id": sender_id,
+                   "gitlab_user_id":  str(user_id["user_id"])}
+        db_url = GITLAB_SERVICE_URL + \
+            "webhooks/user"
+        db_json = json.dumps(db_json)
+        post_request = requests.post(url=db_url, data=db_json, headers=headers)
+        post_request.raise_for_status()
