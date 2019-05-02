b12fcc798227c5449b0927fc5ae65357add280ab
ErickGiffoni <giffoni.erick@gmail.com>
2019-05-02 10:18:51 -0300

    #135 add information about last 7 and last 30 days pipelines to the user

--- ada/actions/get_report.py
+++ ada/actions/get_report.py
@@ -48,7 +48,7 @@ class Report(Action):
                     ["failed_pipelines"], percent_succeded=report_project[0]["pipelines"]["percent_succeded"]))
 
             last_7 = report_project[0]["pipelines"]["recents_pipelines"]["last_7_days"]
-            last_30 = report_project[0]["pipelines"]["recents_pipelines"]["last_30_days"] 
+            last_30 = report_project[0]["pipelines"]["recents_pipelines"]["last_30_days"]
             dispatcher.utter_message("Sobre os últimos 7 dias:\n➡️ Número total de pipelines: {total}\n".format(total=last_7["number_of_pipelines"]) \
                                      + "➡️ {succes} obtiveram sucesso\n➡️ {fail} falharam\n".format(succes=last_7["succeded_pipelines"],
                                                                  fail=last_7["failed_pipelines"])\
@@ -59,7 +59,7 @@ class Report(Action):
                                                                  fail=last_30["failed_pipelines"])\
                                       + "➡️ A porcentagem de sucesso foi: {perc_suc}\n➡️ E a porcentagem de falhas ".format(  perc_suc=last_30["percent_succeded"])\
                                       + "foi: {perc_fail}\n".format(perc_fail=last_30["percent_failed"]))
-            
+
             dispatcher.utter_message("Agora, vou te mostrar a duração dos seus pipelines, trazendo o tempo total, a média, o maior tempo e o menor tempo.")
             dispatcher.utter_message("O tempo total ficou acumulado em {total} segundos. A média de tempo dos pipelines ficou igual a {average} segundos, tendo um tempo máximo de {higher} segundos e um tempo mínimo de {lower} segundos.".format(total=report_project[0]["pipelines_times"]["total"], average=report_project[0]["pipelines_times"]["average"], higher=report_project[0]["pipelines_times"]["higher"], lower=report_project[0]["pipelines_times"]["lower"]))
         except ValueError:
