9f6dbbff26672b5e71e6e131219d140fbcb43639
ErickGiffoni <giffoni.erick@gmail.com>
2019-05-12 19:03:57 -0300

    #194 80% done; missing get user message for which pipeline to rerun, then make new request to ada-gitlab

--- ada/actions/rerun_pipeline.py
+++ ada/actions/rerun_pipeline.py
@@ -15,11 +15,34 @@ class RerunPipeline(Action):
             headers = {'Content-Type': 'application/json'}
             tracker_state = tracker.current_state()
             chat_id = tracker_state["sender_id"]
-            # response = requests.get(GITLAB_SERVICE_URL +
-            #                         "rerun_pipeline/{chat_id}"
-            #                         .format(chat_id=chat_id), headers=headers)
-            # restarted_pipeline = response.json()
-            # TODO HERE
+             response = requests.get(GITLAB_SERVICE_URL +
+                                     "rerun_pipeline/{chat_id}"
+                                     .format(chat_id=chat_id), headers=headers)
+             pipelines = response.json()
+             pipelines_names = []
+             for i in pipelines:
+                 pipelines_names.append(i["names"])
+
+            if pipelines_names:
+                dispatcher.utter_message("Então... encontrei as seguintes\
+                                      pipelines:")
+                dispatcher.utter_message(pipelines_names)
+                dispatcher.utter_message("Qual delas você quer reiniciar ?\
+                                          me fala o nome de uma só viu rsrs")
+                # TODO HERE -> pegar a mensagem do usuario p identificar a
+                # pipeline que ele quer reiniciar
+                # pipeline_to_be_reruned = tracker_state ?????
+
+                # TODO HERE -> fazer a outra requisicao com o id da pipeline
+                # para reinicia-la
+                # response = requests.get(GITLAB_SERVICE_URL +
+                #                    "rerun_pipeline/{chat_id}/{pipeline_id}"
+                #                    .format(chat_id=chat_id, pipeline_id=
+                #                     pipeline_id), headers=headers)
+
+            else :
+                dispatcher.utter_message("Não encontrei nenhuma pipeline\
+                                          no seu projeto :/")
 
         except HTTPError:
             dispatcher.utter_message(
