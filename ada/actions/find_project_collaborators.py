be355eda22b7ef6a2ae16fbf471547f13c72d26b
ErickGiffoni <giffoni.erick@gmail.com>
2019-05-28 15:37:30 -0300

    #234 update find_project_collaborators.py - I believe this is the final version

--- ada/actions/find_project_collaborators.py
+++ ada/actions/find_project_collaborators.py
@@ -6,7 +6,7 @@ from requests.exceptions import HTTPError
 import telegram
 
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "")
-GITLAB_SERVICE_URL = os.getenv("GITLAB_SERVICE_URL", "")
+GITHUB_SERVICE_URL = os.getenv("GITHUB_SERVICE_URL", "")
 
 
 class FindProjectCollaborators(Action):
@@ -15,13 +15,52 @@ class FindProjectCollaborators(Action):
 
     def run(self, dispatcher, tracker, domain):
         try:
-            #TODO
-            pass
+            headers = {'Content-Type': 'application/json'}
+            tracker_state = tracker.current_state()
+            chat_id = tracker_state["sender_id"]
+            # tentar fazer a requisicao
+            response = requests.get(GITHUB_SERVICE_URL + "/api/\
+                                    find_collaborators/{chat_id}".format(
+                                    chat_id=chat_id
+                                    ))
+            response.raise_for_status()
 
-        except blabla :
-            #TODO
-            pass
+        except HTTPError:
+            dispatcher.utter_message(
+                "Ai que pena... não consegui encontrar os contribuidores do \
+                 seu projeto 😔")
+            dispatcher.utter_message(
+                "Vamos tentar de novo daqui a pouco, pode ser ? É só você me \
+                 lembrar !")
+        except ValueError:
+            dispatcher.utter_message(
+                "Estou com problemas para me conectar, me manda "
+                "mais uma mensagem pra ver se dessa vez dá certo.")
+        except NewConnectionError:
+            dispatcher.utter_message(
+                "Estou com problemas para me conectar, me manda "
+                "mais uma mensagem pra ver se dessa vez dá certo.")
 
         else:
-            #TODO
-            pass
+            project_collaborators = json.loads(response) # converte o json em
+            if len(project_collaborators) > 2:           # lista
+                dispatcher.utter_message("Hmmm parece que você não está \
+                                          trabalhando sozinho :)")
+                dispatcher.utter_message("Esses são seus colegas de trabalho:")
+                for item in project_collaborators:
+                    dispatcher.utter_message(item)
+
+            elif len(project_collaborators) == 2:
+                dispatcher.utter_message("Só há 2 pessoas trabalhando no seu \
+                                          projeto:")
+                dispatcher.utter_message("São elas " + project_collaborators[0]
+                                          + "e " + project_collaborators[1])
+
+            elif len(project_collaborators) == 1:
+                dispatcher.utter_message("A princípio só " +
+                                          project_collaborators[0] + "trabalha \
+                                          no seu projeto...")
+
+            else: # ninguém trabalha no projeto
+                dispatcher.utter_message("Que pena...ninguém está contribuindo \
+                                          para o seu projeto :(")
