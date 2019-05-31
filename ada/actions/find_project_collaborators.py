f47d49cdb927ff63556d82bea31ed576c3b2fdc1
ErickGiffoni <giffoni.erick@gmail.com>
2019-05-31 20:39:22 -0300

    #234 add find_project_collaborators action

--- ada/actions/find_project_collaborators.py
+++ ada/actions/find_project_collaborators.py
@@ -19,19 +19,18 @@ class FindProjectCollaborators(Action):
             tracker_state = tracker.current_state()
             chat_id = tracker_state["sender_id"]
             # tentar fazer a requisicao
-            response = requests.get(GITHUB_SERVICE_URL + "/api/\
-                                    find_collaborators/{chat_id}".format(
-                                    chat_id=chat_id
-                                    ))
+            response = requests.get(GITHUB_SERVICE_URL + "api/" +
+                                    "find_collaborators/{chat_id}".format(
+                                    chat_id=chat_id), headers=headers)
             response.raise_for_status()
 
         except HTTPError:
             dispatcher.utter_message(
-                "Ai que pena... não consegui encontrar os contribuidores do \
-                 seu projeto 😔")
+                "Ai que pena... não consegui encontrar os contribuidores do"
+                 " seu projeto 😔")
             dispatcher.utter_message(
-                "Vamos tentar de novo daqui a pouco, pode ser ? É só você me \
-                 lembrar !")
+                "Vamos tentar de novo daqui a pouco, pode ser ? É só você me"
+                 " lembrar !")
         except ValueError:
             dispatcher.utter_message(
                 "Estou com problemas para me conectar, me manda "
@@ -44,23 +43,23 @@ class FindProjectCollaborators(Action):
         else:
             project_collaborators = json.loads(response) # converte o json em
             if len(project_collaborators) > 2:           # lista
-                dispatcher.utter_message("Hmmm parece que você não está \
-                                          trabalhando sozinho :)")
+                dispatcher.utter_message("Hmmm parece que você não está "
+                                          "trabalhando sozinho :)")
                 dispatcher.utter_message("Esses são seus colegas de trabalho:")
                 for item in project_collaborators:
                     dispatcher.utter_message(item)
 
             elif len(project_collaborators) == 2:
-                dispatcher.utter_message("Só há 2 pessoas trabalhando no seu \
-                                          projeto:")
+                dispatcher.utter_message("Só há 2 pessoas trabalhando no seu"
+                                          " projeto:")
                 dispatcher.utter_message("São elas " + project_collaborators[0]
                                           + "e " + project_collaborators[1])
 
             elif len(project_collaborators) == 1:
                 dispatcher.utter_message("A princípio só " +
-                                          project_collaborators[0] + "trabalha \
-                                          no seu projeto...")
+                                          project_collaborators[0] + "trabalha"
+                                          " no seu projeto...")
 
             else: # ninguém trabalha no projeto
-                dispatcher.utter_message("Que pena...ninguém está contribuindo \
-                                          para o seu projeto :(")
+                dispatcher.utter_message("Que pena...ninguém está contribuindo"
+                                          " para o seu projeto :(")
