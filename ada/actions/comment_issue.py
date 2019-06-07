52a41a3bf380cc6f94cde96df73ea8a4a3da7cac
guilherme-mendes <guimendesp12@gmail.com>
2019-06-07 10:56:54 -0300

    #227 Add create comment issues Action

---
+++ ada/actions/comment_issue.py
@@ -0,0 +1,56 @@
+from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
+import os
+import requests
+import json
+from urllib3.exceptions import NewConnectionError
+from requests.exceptions import HTTPError
+import sys
+import telegram
+
+GITHUB_SERVICE_URL = os.getenv("GITHUB_SERVICE_URL", "")
+
+
+class ActionCommentIssue(Action):
+    def name(self):
+        return "action_comment_issue"
+
+    def run(self, dispatcher, tracker, domain):
+        # try:
+        headers = {'Content-Type': 'application/json'}
+        tracker_state = tracker.current_state()
+        chat_id = tracker_state["sender_id"]
+
+        message = tracker.latest_message.get('text')
+        print('#'*30, file=sys.stderr)
+        print(message, file=sys.stderr)
+        print(type(message), file=sys.stderr)
+        print('#'*30, file=sys.stderr)
+
+        splited_message = message.split(": ")
+        issue_body = splited_message[-1]
+        splited_message = splited_message[0].split('#')
+        issue_number = splited_message[-1]
+        data = {"body": issue_body, "issue_number": issue_number}
+        url = GITHUB_SERVICE_URL + "api/comment_issue/{chat_id}".format(
+            chat_id=chat_id)
+        response = requests.post(url=url, data=json.dumps(data),
+                                    headers=headers)
+        print('#'*30, file=sys.stderr)
+        print(response, file=sys.stderr)
+        print('#'*30, file=sys.stderr)
+        text_message = "Certo! Comentei sua issue!"
+        dispatcher.utter_message(text_message)
+        # except HTTPError:
+        #     dispatcher.utter_message(
+        #         "Não consegui criar a issue, tente novamente")
+        # except ValueError:
+        #     dispatcher.utter_message(
+        #         "Estou com problemas para me conectar, me manda "
+        #         "mais uma mensagem pra ver se dessa vez dá certo.")
+        # except NewConnectionError:
+        #     dispatcher.utter_message(
+        #         "Estou com problemas para me conectar, me manda "
+        #         "mais uma mensagem pra ver se dessa vez dá certo.")
+        # else:
+        return [SlotSet('issue_body', issue_body)]
