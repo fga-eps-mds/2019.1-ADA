6b2031cc513dac660480898d3487569b0abb2d65
ErickGiffoni <giffoni.erick@gmail.com>
2019-06-13 11:31:29 -0300

    #263 new way to get issue number by doing a telegram api request...still testing

--- ada/actions/comment_issue.py
+++ ada/actions/comment_issue.py
@@ -8,6 +8,7 @@ from requests.exceptions import HTTPError
 import sys
 
 GITHUB_SERVICE_URL = os.getenv("GITHUB_SERVICE_URL", "")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "")
 
 
 class ActionCommentIssue(Action):
@@ -22,13 +23,45 @@ class ActionCommentIssue(Action):
 
             message = tracker.latest_message.get('text')
 
-            print("**********\nTracker latest message:\n",tracker.latest_message,"\n***********", file=sys.stderr)
-            print("Type do latest_message: ", type(tracker.latest_message),"\n********", file=sys.stderr)
+            #print("**********\nTracker latest message:\n",tracker.latest_message,"\n***********", file=sys.stderr)
+            #print("Type do latest_message: ", type(tracker.latest_message),"\n********", file=sys.stderr)
+            delete_webhook_url = "https://api.telegram.org/bot" + ACCESS_TOKEN + "/deleteWebhook"
+            print("Delete url: ", delete_webhook_url, "\n\n",file=sys.stderr)
+            get_updates_url = "https://api.telegram.org/bot" + ACCESS_TOKEN + "/getUpdates"
+            print("Updates url: ", get_updates_url, "\n\n",file=sys.stderr)
+            peguei_issue_number = False
+            try: # delete webhook
+                delete_response = requests.get(url=delete_webhook_url)
+                delete_response.raise_for_status()
+            except HTTPError:
+                dispatcher.utter_message("Delete webhook falhou")
+            else:
+
+                try: # get updates
+                    get_updates_response = requests.get(url=get_updates_url)
+                    get_updates_response.raise_for_status()
+                except HTTPError:
+                    dispatcher.utter_message("Get updates falhou")
+                else:
+                    get_updates_response = get_updates_response.json()
+                    reply_to_message = get_updates_response["result"][-1]\
+                                       ["message"]["reply_to_message"]
+                    reply_text = reply_to_message["text"]
+                    print("Reply text: ", reply_text, "\n\n", file=sys.stderr)
+                    issue_number = reply_text.split("#")
+                    issue_number = issue_number[1][0]
+                    print("issue number: ", issue_number, "\n\n", file=sys.stderr)
+                    peguei_issue_number = True
 
             splited_message = message.split(": ")
             comment_body = splited_message[-1]
-            splited_message = splited_message[0].split('#')
-            issue_number = splited_message[-1]
+
+            if(not peguei_issue_number):
+                print("Nao peguei issue number pela api do telegram",
+                      file=sys.stderr)
+                splited_message = splited_message[0].split('#')
+                issue_number = splited_message[-1]
+
             data = {"body": comment_body, "issue_number": issue_number}
             url = GITHUB_SERVICE_URL + "api/comment_issue/{chat_id}".format(
                 chat_id=chat_id)
