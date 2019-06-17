abe8f09a512dd2a67a20e220b63c81c14982419f
Caio Fernandes <caiovf13@gmail.com>
2019-06-17 08:03:45 -0300

    #278 Add excepts to action_change_github_repository.py
    Co-authored-By: Bruno Dantas <oliveiradantas96@gmail.com>

--- ada/actions/action_change_github_repository.py
+++ ada/actions/action_change_github_repository.py
@@ -12,10 +12,24 @@ class ChangeGithubRepo(Action):
         return "action_change_github_repository"
 
     def run(self, dispatcher, tracker, domain):
-        headers = {'Content-Type': 'application/json'}
-        tracker_state = tracker.current_state()
-        chat_id = tracker_state["sender_id"]
-        requests.get(GITHUB_SERVICE_URL +
-                     "user/change_repo/{chat_id}"
-                     .format(chat_id=chat_id),
-                     headers=headers)
+        try:
+            headers = {'Content-Type': 'application/json'}
+            tracker_state = tracker.current_state()
+            chat_id = tracker_state["sender_id"]
+            requests.get(GITHUB_SERVICE_URL +
+                        "user/change_repo/{chat_id}"
+                        .format(chat_id=chat_id),
+                        headers=headers)
+        except KeyError:
+            dispatcher.utter_message(
+                "Não consegui encontrar o seu usuário no GitHub, "
+                "por favor verifique ele e me manda novamente.")
+        except IndexError:
+            dispatcher.utter_message(
+                "Não consegui encontrar o seu usuário no GitHub, "
+                "por favor verifique ele e me manda novamente.")
+        except ValueError:
+            dispatcher.utter_message(
+                "Estou com problemas para me conectar, me manda "
+                "mais uma mensagem pra ver se dessa vez dá certo.")
+          
