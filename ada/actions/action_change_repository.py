9827949ca04b93ec09b3f059f44e7a4e49f3a4e4
Caio Fernandes <caiovf13@gmail.com>
2019-06-17 08:05:39 -0300

    #278 Add action_change_repository
    Co-authored-By: Bruno Dantas <oliveiradantas96@gmail.com>

---
+++ ada/actions/action_change_repository.py
@@ -0,0 +1,32 @@
+from rasa_core_sdk import Action
+import requests
+import os
+import telegram
+from telegram import Bot
+import sys
+
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "")
+
+class ChangeRepo(Action):
+    def name(self):
+        return "action_change_repository"
+    
+    def run(self, dispatcher, tracker, domain):
+        bot = Bot(token=ACCESS_TOKEN)
+        buttons = []
+        tracker_state = tracker.current_state()
+        chat_id = tracker_state["sender_id"]
+        git_service = ["Github", "Gitlab"]
+        for name in git_service:
+            print("###"*30 + "\n" + name + "###"*30 + "\n", file=sys.stderr)
+            buttons.append(telegram.InlineKeyboardButton(
+                text=name,
+                callback_data="muda meu repositório do " + name))
+        service_name = [buttons[i:i+2] for i in range(0, len(buttons), 2)]
+        reply_markup = telegram.InlineKeyboardMarkup(service_name)
+        bot.send_message(chat_id=chat_id,
+                         text="Em qual serviço você deseja recadastrar o repositório?",
+                         reply_markup=reply_markup)
+
+
+
